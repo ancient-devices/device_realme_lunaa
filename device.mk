4063fde459c9451a46a110c296832571ceb0d063
gotenksIN <forumomkar@gmail.com>
2022-06-08 05:55:41 +0000

    lunaa: Set default hotspot ssid *Adjusted as per our device Signed-off-by:Bunny <spartha5999@gmail.com>

--- device.mk
+++ device.mk
@@ -55,7 +55,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
-    OPlusSystemUIResTarget
+    OPlusSystemUIResTarget \
+    OPlusWifiResTarget
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
